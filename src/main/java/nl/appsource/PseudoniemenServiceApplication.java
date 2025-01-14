41fe4b9d8c2fc69a3f17a962c6ee47c90289ca6b
Bas Huisman <bhuism@odee.net>
2025-01-14 14:43:59 +0100

    cleanups

--- src/main/java/nl/appsource/PseudoniemenServiceApplication.java
+++ src/main/java/nl/appsource/PseudoniemenServiceApplication.java
@@ -1,6 +1,5 @@
 package nl.appsource;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.NoArgsConstructor;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.security.Security;
 
-@SuppressWarnings({"HideUtilityClassConstructor"})
-@SuppressFBWarnings(value = "EI_EXPOSE_STATIC_REP2",
-    justification = "nl.appsource.PseudoniemenServiceApplication$$SpringCGLIB$$0")
 @SpringBootApplication
 @NoArgsConstructor
 public class PseudoniemenServiceApplication {
@@ -20,7 +16,6 @@ public class PseudoniemenServiceApplication {
     }
 
     public static void main(final String[] args) {
-
         SpringApplication.run(PseudoniemenServiceApplication.class, args);
     }
 }
